3ceb54664384e5cc04ee691a1922dd54eb042e4b
zmaeuropa <smain.rabhi@gmail.com>
2020-06-25 18:02:26 +0200

    add: calculateresult function , and ui vars

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 // Listen for submit
-document.getElementById('load-form').addEventListener('submit', CalculateResults);
+document.getElementById('loan-form').addEventListener('submit', CalculateResults);
+
+// Calculate results function
+function CalculateResults(e) {
+    // UI variables
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+
+
+
+
+    e.preventDefault();
+}
