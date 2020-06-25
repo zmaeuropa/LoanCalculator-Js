846dd7a58095f73967c0bad5a60601d095f35700
zmaeuropa <smain.rabhi@gmail.com>
2020-06-25 18:49:44 +0200

    create: error function and clear error

--- app.js
+++ app.js
@@ -28,8 +28,31 @@ function CalculateResults(e) {
     }
 
 
-
-
-
     e.preventDefault();
+}
+//Show error function
+function showError(error) {
+    //Create div
+    const errorDiv = document.createElement('div');
+
+    //Get elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // Add class
+    errorDiv.className = 'alert alert-danger';
+
+    // Create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // Insert error above heading
+    card.insertBefore(errorDiv, heading);
+
+    // Clear error after 3 seconds
+    setTimeout(clearError, 3000);
+}
+
+// Clear error function
+function clearError() {
+    document.querySelector('.alert').remove();
 }
